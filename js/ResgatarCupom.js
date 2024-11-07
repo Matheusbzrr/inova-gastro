403b1bba5b1fe086a969b2706f41c7803b5265fa
Matheus Bezerra <matheusbzrr3@gmail.com>
2024-11-07 12:35:03 -0300

    feat: conectado com back

---
+++ js/ResgatarCupom.js
@@ -0,0 +1,34 @@
+function ResgatarCupom() {
+    const nome = localStorage.getItem("nome");
+    const email = localStorage.getItem("email");
+  const mensagem =
+    "Parabéns! Você ganhou um super cupom  🎉!!Clique abaixo para resgatar 🤩✌";
+
+  alert(mensagem);
+
+  fetch("http://127.0.0.1:5000/resgatar", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ nome, email, mensagem }),
+  })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Erro na comunicação com o servidor");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      if (data.status === "sucesso") {
+        window.location.href = "Resgatado.html";
+      } else {
+        alert(data.mensagem);
+      }
+    })
+    .catch((error) => {
+      console.error("Erro:", error);
+      alert("Ocorreu um erro ao tentar resgatar o prêmio.");
+    });
+}
